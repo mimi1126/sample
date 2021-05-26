86832428cce2799dbfd08f21f05a8e3bcaf7675f
mimi <mimi.kuma.26@gmail.com>
2021-05-26 15:03:09 +0900

    商品モジュールの作成

---
+++ src/app/mock-products.ts
@@ -0,0 +1,29 @@
+export const products = [
+  {
+    id: 0,
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    heading1: 'head 1',
+    heading2: 'head 2',
+    heading3: 'head 3'
+  },
+  {
+    id: 1,
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    heading1: 'head 1',
+    heading2: 'head 2',
+    heading3: 'head 3'
+  },
+  {
+    id: 2,
+    name: 'Phone Standard',
+    price: 299,
+    description: '',
+    heading1: 'head 1',
+    heading2: 'head 2',
+    heading3: 'head 3'
+  }
+];
