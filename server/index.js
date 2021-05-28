5b9ad673d066b057d191526d9c71dcb2022c19fd
mimi <mimi.kuma.26@gmail.com>
2021-05-28 17:56:48 +0900

    デプロイ環境設定

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
-const config = require('./config/dev')
+const config = require('./config/')
 const FakeDb = require('./fake-db')
 const productRoutes = require('./routes/products')
+const path = require('path')
 const PORT = process.env.PORT || '3001'
 
 
@@ -14,13 +15,22 @@ mongoose.connect(config.DB_URI, {
   useCreateIndex: true
 }).then(
   () => {
-    const fakeDb = new FakeDb()
-    fakeDb.initDb()
+    if (process.env.NODE_ENV !== 'production') {
+      // const fakeDb = new FakeDb()
+      // fakeDb.initDb()
+    }
   }
 )
 
 app.use('/api/v1/products', productRoutes)
 
+if (process.env.NODE_ENV == 'production') {
+  const appPath = path.join(__dirname, '..', 'dist/sample')
+  app.use(express.static(appPath))
+  app.get('*', function(req, res) {
+    res.sendFile(path.resolve(appPath, 'index.html'))
+  })
+}
 
 app.listen(PORT, function() {
   console.log('I am running')
